3f0bd6a1a2dfbc6f2d10ab4291ab5f934fa50b64
James Conallen <jconallen@gmail.com>
2018-12-31 12:07:54 -0500

    moved fcns around

--- c_config-work.js
+++ c_config-work.js
@@ -118,29 +118,19 @@ function getMousePos(canvas, event) {
 }
 
 function controlClick(event) {
-  var x1 = this.width / 2;
-  var y1 = this.height / 2;
   var canvas = event.currentTarget;
   var pos = getMousePos(canvas, event);
-  var x2 = pos.x;
-  var y2 = pos.y;
 
-  //find vector components
-  var A1x = x1;
-  var A1y = y1;
-  var A2x = x2;
-  var A2y = y2;
-  var B1x = 0;
-  var B1y = x1;
-  var B2x = this.width;
-  var B2y = y1;
+  var ax1 = this.width / 2;
+  var ay1 = this.height / 2;
+  var ax2 = pos.x;
+  var ay2 = pos.y;
 
-  var dAx = A2x - A1x;
-  var dAy = A2y - A1y;
-  var dBx = B2x - B1x;
-  var dBy = B2y - B1y;
-  var theta = Math.atan2(dAx * dBy - dAy * dBx, dAx * dBx + dAy * dBy);
-  //if(angle < 0) {angle = angle * -1;}
+  var adx = ax2 - ax1;
+  var ady = ay2 - ay1;
+  var bdx = this.width;
+  var bdy = ay1 - ax1;
+  var theta = Math.atan2(adx * bdy - ady * bdx, adx * bdx + ady * bdy);
 
   if (canvas.id == "shoulderControl") {
     shoulderAngle = theta;
@@ -221,4 +211,240 @@ function clicked(event) {
 
   // update plot
   drawConfiguration();
+}
+
+function _cx(wrX) {
+  return (wrX * canvasWidth) / worldWidth;
+}
+
+function _cy(wrY) {
+  return canvasHeight - (wrY * canvasHeight) / worldHeight;
+}
+
+function _wx(cvX) {
+  return (cvX * worldWidth) / canvasWidth;
+}
+
+function _wy(cvY) {
+  return (worldHeight * (canvasHeight - cvY)) / canvasHeight;
+}
+
+function _block(wx, wy) {
+  var x1 = _cx(wx);
+  var y1 = _cy(wy);
+  var w = canvasWidth / worldWidth;
+  var h = -canvasHeight / worldHeight;
+  ctxWorkspace.save();
+  ctxWorkspace.fillStyle = "darkgray";
+  ctxWorkspace.fillRect(x1, y1, w, h);
+  ctxWorkspace.restore();
+}
+
+function rectangle(p1x, p1y, p2x, p2y, color) {
+  ctxWorkspace.save();
+  ctxWorkspace.strokeStyle = color;
+  var x1 = _cx(p1x);
+  var y1 = _cy(p1y);
+  var w = _cx(p2x) - x1;
+  var h = _cy(p2y) - y1;
+  ctxWorkspace.strokeRect(x1, y1, w, h);
+  ctxWorkspace.restore();
+}
+
+function circle(wx, wy, wr, color) {
+  ctxWorkspace.save();
+  ctxWorkspace.beginPath();
+  var x = _cx(wx);
+  var y = _cy(wy);
+  var r = _cx(wr); // since a circle doesn't matter skew
+  ctxWorkspace.strokeStyle = color;
+  ctxWorkspace.arc(x, y, r, 0, 2 * Math.PI, false);
+  ctxWorkspace.stroke();
+  ctxWorkspace.restore();
+}
+
+function line(px1, py1, px2, py2, color) {
+  var x1 = _cx(px1);
+  var y1 = _cy(py1);
+  var x2 = _cx(px2);
+  var y2 = _cy(py2);
+  ctxWorkspace.save();
+  ctxWorkspace.strokeStyle = color;
+  ctxWorkspace.beginPath();
+  ctxWorkspace.moveTo(x1, y1);
+  ctxWorkspace.lineTo(x2, y2);
+  ctxWorkspace.stroke();
+  ctxWorkspace.restore();
+}
+
+function point(wx, wy, color) {
+  ctxWorkspace.save();
+  ctxWorkspace.beginPath();
+  var x = _cx(wx);
+  var y = _cy(wy);
+  ctxWorkspace.strokeStyle = color;
+  ctxWorkspace.fillStyle = color;
+  ctxWorkspace.arc(x, y, 2, 0, 2 * Math.PI, false);
+  ctxWorkspace.fill();
+  ctxWorkspace.stroke();
+  ctxWorkspace.restore();
+}
+
+function drawArm(a, color) {
+  var cx1 = _cx(a.x1);
+  var cy1 = _cy(a.y1);
+  var cx2 = _cx(a.x2);
+  var cy2 = _cy(a.y2);
+
+  ctxWorkspace.save();
+  ctxWorkspace.strokeStyle = color;
+  ctxWorkspace.beginPath();
+  ctxWorkspace.moveTo(cx1, cy1);
+  ctxWorkspace.lineTo(cx2, cy2);
+  ctxWorkspace.stroke();
+  ctxWorkspace.restore();
+
+  ctxWorkspace.beginPath();
+  ctxWorkspace.strokeStyle = "red";
+  ctxWorkspace.fillStyle = "red";
+  ctxWorkspace.arc(cx2, cy2, 2, 0, 2 * Math.PI, false);
+  ctxWorkspace.fill();
+  ctxWorkspace.stroke();
+}
+
+function animate() {
+  drawNext();
+  globalID = requestAnimationFrame(animate);
+}
+
+function drawNext() {
+  if (elbowAngle > Math.PI * 2) {
+    elbowAngle = 0;
+  } else {
+    elbowAngle = elbowAngle + deltaElbowDirection * deltaElbowAngle;
+  }
+  if (shoulderAngle > Math.PI * 2) {
+    shoulderAngle = 0;
+  } else {
+    shoulderAngle = shoulderAngle + deltaShoulderDirection * deltaShoulderAngle;
+  }
+
+  animateArm();
+}
+
+function animateArm() {
+  ctxWorkspace.clearRect(0, 0, canWorkspace.width, canWorkspace.height);
+  drawWorkspace();
+  // put in blocks
+  for (b = 0; b < blocks.length; b++) {
+    var block = blocks[b];
+    _block(block.x1, block.y1);
+  }
+
+  var bs = arm(worldWidth / 2, 0, Math.PI / 2, baseLength);
+  var sh = arm(bs.x2, bs.y2, shoulderAngle, shoulderLength);
+  var el = arm(sh.x2, sh.y2, elbowAngle, elbowLength);
+
+  if (hitWall(sh) || hitAnyBlock(sh)) {
+    shoulderAngle = shoulderAngle - deltaShoulderDirection * deltaShoulderAngle; // back off
+    sh = arm(bs.x2, bs.y2, shoulderAngle, shoulderLength);
+    deltaShoulderDirection = -deltaShoulderDirection;
+  } else if (hitWall(el) || hitAnyBlock(el)) {
+    elbowAngle = elbowAngle - deltaElbowDirection * deltaElbowAngle; // back off
+    el = arm(sh.x2, sh.y2, elbowAngle, elbowLength);
+    deltaElbowDirection = -deltaElbowDirection; // change direction
+
+    shoulderAngle = shoulderAngle - deltaShoulderDirection * deltaShoulderAngle; // back shoulder too
+    sh = arm(bs.x2, bs.y2, shoulderAngle, shoulderLength);
+  }
+
+  drawArm(bs, "black");
+  drawArm(sh, "blue");
+  drawArm(el, "green");
+  drawControls();
+}
+
+function drawConfiguration() {
+  for (var x = 0; x < canConfiguration.width; x++) {
+    for (var y = 0; y < canConfiguration.height; y++) {
+      var s =
+        ((x - canConfiguration.width / 2) * 2 * Math.PI) /
+        canConfiguration.width;
+      var e =
+        ((y - canConfiguration.height / 2) * 2 * Math.PI) /
+        canConfiguration.height;
+
+      var bs = arm(worldWidth / 2, 0, Math.PI / 2, baseLength, "black");
+      var sh = arm(bs.x2, bs.y2, s, shoulderLength, "blue");
+      var el = arm(sh.x2, sh.y2, e, elbowLength, "green");
+
+      var color = "lightgray";
+      if (hitWall(el)) {
+        color = "red";
+      } else if (hitAnyBlock(sh)) {
+        color = "blue";
+      } else if (hitAnyBlock(el)) {
+        color = "green";
+      }
+
+      ctxConfiguration.save();
+      ctxConfiguration.beginPath();
+      ctxConfiguration.fillStyle = color;
+      ctxConfiguration.fillRect(x, y, 1, 1);
+      ctxConfiguration.fill();
+      ctxConfiguration.restore();
+    }
+  }
+  ctxConfiguration.save();
+  ctxConfiguration.beginPath();
+  var x = (canConfiguration.width * (shoulderAngle + Math.PI)) / (2 * Math.PI);
+  var y = (canConfiguration.height * (elbowAngle + Math.PI)) / (2 * Math.PI);
+
+  ctxConfiguration.strokeStyle = "black";
+  ctxConfiguration.fillStyle = "black";
+  ctxConfiguration.arc(x, y, 2, 0, 2 * Math.PI, false);
+  ctxConfiguration.fill();
+  ctxConfiguration.stroke();
+  ctxConfiguration.restore();
+}
+
+function drawWorkspace() {
+  drawGridLines();
+  circle(worldWidth / 2, baseLength, shoulderLength, "lightgray");
+}
+
+function drawGridLines() {
+  var dx = canvasWidth / worldWidth;
+  var dy = canvasHeight / worldHeight;
+
+  ctxWorkspace.strokeStyle = "lightblue";
+  ctxWorkspace.lineWidth = 1;
+
+  var ticks;
+
+  ctxWorkspace.save();
+  ctxWorkspace.beginPath();
+
+  ticks = Math.floor(canWorkspace.width / dx);
+
+  for (var i = 1; i <= ticks; i++) {
+    var x = i * dx;
+    ctxWorkspace.moveTo(x, 0);
+    ctxWorkspace.lineTo(x, canWorkspace.height);
+    ctxWorkspace.stroke();
+  }
+
+  ticks = Math.floor(canWorkspace.height / dy);
+
+  for (var i = 1; i <= ticks; i++) {
+    var y = i * dy;
+    ctxWorkspace.moveTo(0, y);
+    ctxWorkspace.lineTo(canWorkspace.width, y);
+    ctxWorkspace.stroke();
+  }
+
+  ctxWorkspace.closePath();
+  ctxWorkspace.restore();
+
+  return;
 }
